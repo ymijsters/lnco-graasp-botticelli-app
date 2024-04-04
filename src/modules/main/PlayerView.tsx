ef95461d54c569ec6c7390537965e794e1d0ad65
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2024-04-04 15:15:53 +0200

    feat: make height full viewport

--- src/modules/main/PlayerView.tsx
+++ src/modules/main/PlayerView.tsx
@@ -1,6 +1,8 @@
-// import { Box, Typography } from '@mui/material';
+// import { Typography } from '@mui/material';
 // import { useLocalContext } from '@graasp/apps-query-client';
 // import { hooks } from '@/config/queryClient';
+import { Box } from '@mui/material';
+
 import { PLAYER_VIEW_CY } from '@/config/selectors';
 import ParticipantInteraction from '@/modules/interaction/ParticipantInteraction';
 
@@ -9,13 +11,13 @@ const PlayerView = (): JSX.Element => (
   // const { data: appContext } = hooks.useAppContext();
   // const members = appContext?.members;
 
-  <div data-cy={PLAYER_VIEW_CY}>
+  <Box data-cy={PLAYER_VIEW_CY} sx={{ height: '100vh' }}>
     {/* Player as {permission} */}
     {/* <Box p={2}> */}
     {/*   <Typography>Members</Typography> */}
     {/*   <pre>{JSON.stringify(members, null, 2)}</pre> */}
     {/* </Box> */}
     <ParticipantInteraction />
-  </div>
+  </Box>
 );
 export default PlayerView;
